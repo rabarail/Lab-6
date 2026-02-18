a94758434252eaeaacf253befd5e6ee9358334de
Rajani <rajani.kami004@gmail.com>
2026-02-18 15:27:28 -0500

    Defining dictionary

--- Lab6_rbaraili-1.py
+++ Lab6_rbaraili-1.py
@@ -4,4 +4,11 @@ Purpose: simulates a user login using dictionary to store usernames and password
 Starter code: None
 Date:02/18/2026"""
 
+users = {
+    "guest": "guest",
+    "rajani": "password1",
+    "sana": "twice",
+    "gwalters": "S3curePass!",
+    "user1": "password2"
+}
 
