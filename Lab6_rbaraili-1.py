610f6a6e95e87ab7737bd5fe6d3a44c7ef4dcf43
Rajani <rajani.kami004@gmail.com>
2026-02-18 15:25:53 -0500

    Begining new lab

---
+++ Lab6_rbaraili-1.py
@@ -0,0 +1,7 @@
+"""Program name: User login system
+Author: Rajani Baraili
+Purpose: simulates a user login using dictionary to store usernames and passwords.
+Starter code: None
+Date:02/18/2026"""
+
+
